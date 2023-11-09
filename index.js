5ccb73a7d08465fd8932102724ccd611dd54669e
christian-gerard <1@christiangerard.me>
2023-11-09 14:01:44 -0700

    Add for loops

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(name) {
+    const thankYouMessages = [];
+    for(let i = 0; i < name.length; i++) {
+        thankYouMessages[i] =  `Thank you, ${name[i]}, for the wonderful surprise gift!`
+        
+    }
+    return thankYouMessages
+
+}
+
+function countDown(number) {
+    for(let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+
+    
+}
+
